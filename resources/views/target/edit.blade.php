44c7f8ca77de144bc0c7dc1d751514b1c347ce19
SultanGyar <sultangyarofficial@gmail.com>
2023-08-23 13:25:01 +0700

    Memperbaiki CRUD Target

--- resources/views/target/edit.blade.php
+++ resources/views/target/edit.blade.php
@@ -1,11 +1,12 @@
 @extends('adminlte::page')
-@section('title', 'Tambah Target')
-@section('content_header')
-<h1 class="m-0 text-dark">Tambah Target</h1>
+@section('title', 'Edit Data')
 
+@section('content_header')
+<h1 class="m-0 text-dark">Edit Data Target</h1>
 @stop
 @section('content')
-<form action="{{route('target.store')}}" method="post">
+<form action="{{route('target.update', $target)}}" method="post">
+    @method('PUT')
     @csrf
     <div class="row">
         <div class="col-12">
@@ -16,39 +17,35 @@
                         <select class="form-control @error('target_proses') is-invalid @enderror" id="target_proses"
                             name="target_proses">
                             <option value="" hidden>Pilih Proses</option>
-                            <option value="Winding" @if(old('target_proses')=='Winding' ) selected @endif>Winding
-                            </option>
-                            <option value="Power Press" @if(old('target_proses')=='Power Press' ) selected @endif>Power
-                                Press
-                            </option>
-                            <option value="Assembling" @if(old('target_proses')=='Assembling' ) selected @endif>
-                                Assembling
-                            </option>
+                            <option value="Winding" @if($target->target_proses == 'Winding' || old('target_proses')=='Winding' ) selected @endif>Winding</option>
+                            <option value="Power Press" @if($target->target_proses == 'Power Press' || old('target_proses')=='Power Press' ) selected @endif>Power Press</option>
+                            <option value="Assembling" @if($target->target_proses == 'Assembling' || old('target_proses')=='Assembling' ) selected @endif>Assembling</option>
                         </select>
                         @error('target_proses')
                         <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-
+                    
                     <div class="form-group">
                         <label for="tanggal_target">Tanggal</label>
                         <input type="date" class="form-control @error('tanggal_target') is-invalid @enderror"
                             id="tanggal_target" placeholder="tanggal_target" name="tanggal_target"
-                            value="{{ old('tanggal_target') ?? date('Y-m-d') }}">
+                            value="{{$target->tanggal_target ?? old('tanggal_target') ?? date('Y-m-d') }}">
                         @error('tanggal_target')
                         <span class="text-danger">{{ $message }}</span>
                         @enderror
                     </div>
-
+                    
                     <div class="form-group">
-                        <label for="target_quantity_byadmin">Quantity</label>
+                        <label for="target_quantity_byadmin">Target Quantity</label>
                         <input type="number" class="form-control @error('target_quantity_byadmin') is-invalid @enderror"
                             id="target_quantity_byadmin" placeholder="Quantity" name="target_quantity_byadmin"
-                            value="{{ old('target_quantity_byadmin') }}">
+                            value="{{$target->target_quantity_byadmin ?? old('target_quantity_byadmin') }}">
                         @error('target_quantity_byadmin')
                         <span class="text-danger">{{ $message }}</span>
                         @enderror
-                    </div>
+                    </div>                    
+            
                 </div>
                 <div class="card-footer">
                     @if($errors->any())
--- resources/views/target/index.blade.php
+++ resources/views/target/index.blade.php
@@ -1,9 +1,14 @@
 @extends('adminlte::page')
 @section('title', 'Target Quantity')
 @section('content_header')
-<h1 class="m-0 text-dark"> Quantity </h1>
+<h1 class="m-0 text-dark">Target Quantity </h1>
 @stop
 @section('content')
+@if(session('warning_message'))
+    <div class="alert alert-warning">
+        {{ session('warning_message') }}
+    </div>
+@endif
 <div class="row">
     <div class="col-12">
         <div class="card">
@@ -12,7 +17,12 @@
                     <h4 class="card-title-center text-center text-dark">Target Quantity</h4>
                 </div>
                 <p></p>
-                <a href="{{ route('target.create') }}" class="btn btn-sm btn-primary mb-2">Tambah</a>
+                <div class="btn-group mb-2">
+                    <a href="{{ route('target.create') }}" class="text-btn-center btn btn-md btn-primary"
+                        style="height: 38px;">Tambah</a>
+
+                    <input type="month" class="form-control ml-2" id="selectBulan">
+                </div>
                 <div class="table-responsive">
                     <table class="table table-hover table-bordered table-striped" id="example2">
                         <thead>
@@ -30,6 +40,10 @@
                                 <td>{{ $data->tanggal_target }}</td>
                                 <td>{{ $data->target_quantity_byadmin }}</td>
                                 <td>
+                                    <a href="{{ route('target.edit', $data) }}" class="btn btn-primary btn-xs">
+                                        Edit
+                                    </a>
+
                                     <a href="{{ route('target.destroy', $data->id) }}"
                                         onclick="notificationBeforeDelete(event, this)" class="btn btn-danger btn-xs">
                                         Delete
@@ -53,14 +67,64 @@
 <script>
     $('#example2').DataTable({
     "responsive": true,
-});
+    });
 
-function notificationBeforeDelete(event, el) {
-    event.preventDefault();
-    if (confirm('Apakah anda yakin akan menghapus data ? ')) {
-        $("#delete-form").attr('action', $(el).attr('href'));
-        $("#delete-form").submit();
+    function notificationBeforeDelete(event, el) {
+        event.preventDefault();
+        if (confirm('Apakah anda yakin akan menghapus data ? ')) {
+            $("#delete-form").attr('action', $(el).attr('href'));
+            $("#delete-form").submit();
+        }
     }
-}
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const selectBulan = document.getElementById("selectBulan");
+
+        // Mendapatkan tanggal saat ini
+        const today = new Date();
+        const currentYear = today.getFullYear();
+        const currentMonth = today.getMonth() + 1; // Bulan dimulai dari 0 (Januari) - 11 (Desember)
+
+        // Set nilai bulan saat ini pada input
+        selectBulan.value = `${currentYear}-${String(currentMonth).padStart(2, '0')}`;
+
+        // Memfilter data berdasarkan bulan yang dipilih saat halaman dimuat
+        const selectedDate = new Date(selectBulan.value);
+        const selectedYear = selectedDate.getFullYear();
+        const selectedMonth = selectedDate.getMonth() + 1;
+        filterDataByMonth(selectedYear, selectedMonth);
+
+        // Fungsi untuk memfilter data berdasarkan bulan
+        selectBulan.addEventListener("change", function () {
+            const selectedDate = new Date(selectBulan.value);
+            const selectedYear = selectedDate.getFullYear();
+            const selectedMonth = selectedDate.getMonth() + 1;
+
+            // Memfilter data berdasarkan bulan yang dipilih
+            filterDataByMonth(selectedYear, selectedMonth);
+        });
+
+        // Fungsi untuk memfilter data berdasarkan bulan yang dipilih
+        function filterDataByMonth(year, month) {
+            const rows = document.querySelectorAll('#example2 tbody tr');
+
+            rows.forEach(row => {
+                const dataTahun = row.cells[1].textContent.split('-')[0];
+                const dataBulan = row.cells[1].textContent.split('-')[1];
+
+                if (parseInt(dataTahun) === year && parseInt(dataBulan) === month) {
+                    row.style.display = 'table-row';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+            
+            // Panggil ulang DataTable untuk merender ulang tabel
+            $('#example2').DataTable().destroy();
+            $('#example2').DataTable({
+                "responsive": true,
+            });
+        }
+    });
 </script>
 @endpush
